73285ff678b52a1af993cbb8fa4f99f1b421d940
ham-am-99 <68792323+ham-am-99@users.noreply.github.com>
2021-06-23 11:41:47 +0100

    Update PageDeRecherche.js

--- PageDeRecherche.js
+++ PageDeRecherche.js
@@ -10,10 +10,63 @@ import {
 } from 'react-native';
 
 type Props = {};
-
+function urlPourRequete(valeur) { 
+return 'https://restcountries.eu/rest/v2/name/' 
+ + valeur; 
+ 
+}
 export default class PageDeRecherche extends Component<Props> {
+  
+  constructor(props) { 
+   
+ super(props); 
+ this.state = { 
+ requeteDeRecherche: 'morocco',
+ estEnChargement: false,
+ message: '',
+
+ 
+ 
+}; 
+ 
+}
+
+_auChangementDeLaRecherche = (event) => { 
+  
+
+ this.setState({ requeteDeRecherche: event.nativeEvent.text }); 
+ 
+ 
+};
+_executerRequete = (requete) => { 
+ console.log(requete); 
+ this.setState({ estEnChargement: true }); 
+ fetch(requete) 
+ .then(reponse => reponse.json()) 
+ .then(json => this._gererLaReponse(json)) 
+ .catch(error => 
+ this.setState({ 
+ estEnChargement: false, 
+ message: 'Quelque chose de mauvais s\'est produit' + error 
+ }));
+}; 
+_auDemarrageDeLaRecherche = () => { 
+ const requete = urlPourRequete(this.state.requeteDeRecherche); 
+ this._executerRequete(requete); 
+};
+ _gererLaReponse = (reponse) => { 
+ this.setState({ estEnChargement: false, message: '' }); 
+ console.log('Nombre de pays trouvés :' + reponse.length); 
+ };
+
     render() {
+      
+  const indicateurDeChargement = this.state.estEnChargement ? 
+ <ActivityIndicator size='large' color='0000ff'/> : null;
+     
         return (
+          
+          
         <View style={styles.conteneur}>
             <Text style={styles.description}>
             Rechercher des pays à explorer !
@@ -25,14 +78,19 @@ export default class PageDeRecherche extends Component<Props> {
                 <TextInput
                 underlineColorAndroid={'transparent'}
                 style={styles.requeteEntree}
+                 onChange={this._auChangementDeLaRecherche}
+                
                 placeholder='Rechercher par nom de pays'/>
                 <Button
-                onPress={() => {}}
+               onPress = {this._auDemarrageDeLaRecherche}
                 color='#48AAEC'
                 title='Démarrer'
                 />
             </View> 
+           
             <Image source={require('./Ressources/pays.png')} style={styles.image}/>
+            {indicateurDeChargement}
+            <Text style={styles.description}>{this.state.message}</Text>
         </View>
         );
     }
